a0883ef8925dd48ba65890e857ec46aa2587ac3d
Chad McElligott <chad.mcelligott@gmail.com>
2016-12-03 23:24:41 -0600

    Set 0600 permissions on test_credentials pre-test
    The RSpec tests utilize the `test_credentials` file as a sample
    credential file. Git does not manage granular file permissions,
    only the execute and symlink bits. So in order to give this file
    the 0600 permissions that rubygems wants it to have, this patch
    forces that permission prior to running the tests.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,3 +2,7 @@ $:.unshift(File.dirname(__FILE__) + '/../../lib')
 
 require 'rspec/mocks'
 require 'keycutter'
+
+# Set permissions to what rubygems wants,
+# so it doesn't throw a permissions error
+File.chmod(0600, 'test_credentials')
